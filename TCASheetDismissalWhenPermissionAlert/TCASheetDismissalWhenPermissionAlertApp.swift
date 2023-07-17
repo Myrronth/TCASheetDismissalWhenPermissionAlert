ad6912cfc1739417faeb84f81c3241f49ee2e9bb
Tobias Kreß <tobias.kress@intive.com>
2023-07-17 08:44:51 +0200

    Initial Commit

---
+++ TCASheetDismissalWhenPermissionAlert/TCASheetDismissalWhenPermissionAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASheetDismissalWhenPermissionAlertApp.swift
+//  TCASheetDismissalWhenPermissionAlert
+//
+//  Created by Tobias Kreß on 17.07.23.
+//
+
+import SwiftUI
+
+@main
+struct TCASheetDismissalWhenPermissionAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
